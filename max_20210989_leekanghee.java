bc39f8f3b178e0dc202934f480986b2709135563
lkh409 <72863423+lkh409@users.noreply.github.com>
2023-09-18 11:54:36 +0900

    Update array.java

--- max_20210989_leekanghee.java
+++ max_20210989_leekanghee.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 public class max_20210989_leekanghee {
-	private static final int r_max = 100;
+	private static final int r_max = 100;   //12줄에서 +1이면,, 3줄 r_max가 99가 되어야 하는가..?
 
 	public static void main(String[] args) {
 		int[] array = new int[5];
